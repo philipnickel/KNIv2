d31d82aaecb2fce03d9630dfffe034e531f0832b
philipnickel <tipkik@hotmail.com>
2025-09-17 14:51:21 +0200

    Serve media files when DEBUG is false

--- kni_app/KNI/urls.py
+++ kni_app/KNI/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
+from django.views.static import serve as serve_static
+import re
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -18,7 +20,14 @@ urlpatterns = [
 
 
 if settings.MEDIA_URL and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Normalize the media prefix and register explicit serving patterns for production
+    media_prefix = settings.MEDIA_URL.lstrip("/")
+    if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    else:
+        urlpatterns += [
+            re_path(r"^%s(?P<path>.*)$" % re.escape(media_prefix), serve_static, {"document_root": settings.MEDIA_ROOT}),
+        ]
 
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
